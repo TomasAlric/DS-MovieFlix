29624950e5fc438cfaccd778f88e4454f2296c67
Tomas Alric <tomas.alricc@gmail.com>
2022-10-21 01:32:48 -0300

    Criando services

---
+++ bds06/src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,62 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Transactional(readOnly = true)
+    public List<ReviewDTO> reviewsForCurrentUser(Long id) {
+
+        Optional<Movie> obj = movieRepository.findById(id);
+        Movie movie = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+
+        User user = authService.authenticated();
+        List<Review> list = reviewRepository.find(user, id);
+        return list.stream().map(x -> new ReviewDTO(x, user)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public ReviewDTO insert(ReviewDTO dto) {
+
+        User user = authService.authenticated();
+
+        Review entity = new Review();
+        copyDtoToEntity(dto, entity);
+        entity = reviewRepository.save(entity);
+        return new ReviewDTO(entity, user);
+    }
+
+    private void copyDtoToEntity(ReviewDTO dto, Review entity) {
+        Movie movie = movieRepository.getOne(dto.getMovieId());
+        User user = authService.authenticated();
+        authService.validateSelfOrMember(user.getId());
+
+        entity.setText(dto.getText());
+        entity.setMovie(movie);
+        entity.setUser(user);
+    }
+}
