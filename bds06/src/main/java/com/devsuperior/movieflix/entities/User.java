bf4322cf565fc83fade66836988224c712380aab
Tomas Alric <tomas.alricc@gmail.com>
2022-10-21 01:34:21 -0300

    Todos os testes passando

--- bds06/src/main/java/com/devsuperior/movieflix/entities/User.java
+++ bds06/src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -117,10 +117,11 @@ public class User implements Serializable, UserDetails {
         return true;
     }
 
-    public boolean hasHole(String roleName) {
+    public boolean hasRole(String roleName) {
         for (Role role : roles) {
             if (role.getAuthority().equals(roleName)) {
                 return true;
+
             }
         }
         return false;
